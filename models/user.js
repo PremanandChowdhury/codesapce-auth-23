20fa6ec24f1e404d13de5464b745a1c1a2cdb308
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-06 11:54:01 +0000

    created user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    default: null
+  },
+  lastName: {
+    type: String,
+    default: null
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  token: {
+    type: String,
+    default: null
+  }
+})
+
+module.exports = mongoose.model('user', userSchema)
