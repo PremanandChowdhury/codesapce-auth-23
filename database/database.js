94cb3c4dc0a375f3f2eec9fc6545112a9e2485d2
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-06 09:16:19 +0000

    MongoDB atlas creds added

---
+++ database/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const { MONGODB_ATLAS_URI } = process.env
+
+exports.connect = () => {
+  mongoose.connect(MONGODB_ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {})
+  .catch((err) => {
+    console.log(`DB connection FAILED`);
+    console.log(err);
+    process.exit(1)
+  })
+}
