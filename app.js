c23bac44bbd9eb94486af4c35898ea9deac6241d
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-06 07:38:51 +0000

    syntax change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-import express from 'express'
+const express = require('express')
 
 const app = express()
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import app from './app'
-const { PORT } = process.env
+const app = require('./app')
+const {PORT} = process.env
 
 app.listen( PORT, () => {
   console.log(`Server is running on PORT ${PORT}`)
